59c671d488c069f44fc39d7b90fae65589e360de
Aditi <cs1140205@iitd.ac.in>
2017-09-14 18:35:29 +0530

    added api names for 3.1

---
+++ diskVirtual.py
@@ -0,0 +1,26 @@
+
+
+block_size = 100
+blocks_diskA = []
+blocks_diskB = []
+
+def getDiskBlocks():
+	return []
+
+def writeBlock(block_no, write_data):
+	x = 100
+
+def readBlock(block_no):
+	y = 1
+
+def createDisk(id, num_blocks):
+	print "Create..."
+
+def readDiskBlock(id, block_no):
+	print "Reading disk block..."
+
+def writeDiskBlock(id, block_no, write_data):
+	print "Writing disk block..."
+
+def deleteDisk(id):
+	print "Deleting disk..."
