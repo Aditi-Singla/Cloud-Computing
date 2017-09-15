7f12d375c13a448cb1f8d27f626960c04267bc62
Aditi-Singla <aditisksingla@gmail.com>
2017-09-16 00:35:09 +0530

    Test added

--- testScript.py
+++ testScript.py
@@ -1,3 +1,5 @@
+import diskVirtual
+
 def Test1():
     # Disk = VirtualDisk(200, 300)
     # Disk.CallCmd("createDisk", 1, {'size':100})
@@ -14,45 +16,53 @@ def Test1():
     # print 'Read', Disk.CallCmd("readFromDisk", 2, {'blockNo':10})
     print "Reading disk 2 : ", readDiskBlock(2, 10)
     # print 'Read', Disk.CallCmd("readFromDisk", 1, {'blockNo':5})
-    print "reading disk 1 : ", readDiskBlock(1, 5)
-    Disk.CallCmd("writeToDisk", 1, {'blockNo':5, 'data':'Six'})
-    print 'Read', Disk.CallCmd("readFromDisk", 1, {'blockNo':5})
-    Disk.CallCmd('deleteDisk', 2, {})
+    print "Reading disk 1 : ", readDiskBlock(1, 5)
+    #Disk.CallCmd("writeToDisk", 1, {'blockNo':5, 'data':'Six'})
+    print "Writing to disk 1 : ", writeDiskBlock(1, 5, "Six")
+    #print 'Read', Disk.CallCmd("readFromDisk", 1, {'blockNo':5})
+    print "Reading disk 1 : ", readDiskBlock(1,5)
+    #Disk.CallCmd('deleteDisk', 2, {})
+    print "Deleting disk 2 : ", deleteDisk(2)
     # Disk.CallCmd("readFromDisk", 1, {'blockNo':10})
     # Disk.CallCmd("", 2, {'size':200})
-    printState(Disk)
-    Disk.CallCmd('deleteDisk', 1, {})
-    printState(Disk)
+    #printState(Disk)
+    print disks
+    #Disk.CallCmd('deleteDisk', 1, {})
+    print "Deleting disk 1 : ", deleteDisk(1)
+    #printState(Disk)
+    print disks
 
-def Test2():
-    Disk = VirtualDisk(200, 300)
-    Disk.CallCmd("createDisk", 1, {'size':100})
-    Disk.CallCmd("createDisk", 2, {'size':200})
-    Disk.CallCmd("writeToDisk", 1, {'blockNo':5, 'data':'Five'})
-    Disk.CallCmd("writeToDisk", 2, {'blockNo':10, 'data':'Ten'})
 
-    printState(Disk)
-    # Disk.CallCmd("readFromDisk", 2, {'blockNo':5})
-    print 'Read', Disk.CallCmd("readFromDisk", 2, {'blockNo':10})
-    print 'Read', Disk.CallCmd("readFromDisk", 1, {'blockNo':5})
+Test1()
+# def Test2():
+#     Disk = VirtualDisk(200, 300)
+#     Disk.CallCmd("createDisk", 1, {'size':100})
+#     Disk.CallCmd("createDisk", 2, {'size':200})
+#     Disk.CallCmd("writeToDisk", 1, {'blockNo':5, 'data':'Five'})
+#     Disk.CallCmd("writeToDisk", 2, {'blockNo':10, 'data':'Ten'})
 
-    snap = Disk.CallCmd("snapShot", 1, {})
-    Disk.CallCmd("writeToDisk", 1, {'blockNo':5, 'data':'Six'})
-    print 'Read', Disk.CallCmd("readFromDisk", 1, {'blockNo':5})
+#     printState(Disk)
+#     # Disk.CallCmd("readFromDisk", 2, {'blockNo':5})
+#     print 'Read', Disk.CallCmd("readFromDisk", 2, {'blockNo':10})
+#     print 'Read', Disk.CallCmd("readFromDisk", 1, {'blockNo':5})
 
-    snap2 = Disk.CallCmd("snapShot", 2, {})
-    Disk.CallCmd("writeToDisk", 2, {'blockNo':11, 'data':'Eleven'})
-    printState(Disk)
+#     snap = Disk.CallCmd("snapShot", 1, {})
+#     Disk.CallCmd("writeToDisk", 1, {'blockNo':5, 'data':'Six'})
+#     print 'Read', Disk.CallCmd("readFromDisk", 1, {'blockNo':5})
 
-    print 'Rolling Back 2'
-    Disk.CallCmd('rollBack', 2, {'snapShotNo':snap2})
-    printState(Disk)
-    print 'Rolling Back 1'
-    Disk.CallCmd('rollBack', 1, {'snapShotNo':snap})
-    printState(Disk)
+#     snap2 = Disk.CallCmd("snapShot", 2, {})
+#     Disk.CallCmd("writeToDisk", 2, {'blockNo':11, 'data':'Eleven'})
+#     printState(Disk)
 
-    Disk.CallCmd('deleteDisk', 2, {})
-    # Disk.CallCmd("readFromDisk", 1, {'blockNo':10})
-    # Disk.CallCmd("", 2, {'size':200})
-    Disk.CallCmd('deleteDisk', 1, {})
-    printState(Disk)
+#     print 'Rolling Back 2'
+#     Disk.CallCmd('rollBack', 2, {'snapShotNo':snap2})
+#     printState(Disk)
+#     print 'Rolling Back 1'
+#     Disk.CallCmd('rollBack', 1, {'snapShotNo':snap})
+#     printState(Disk)
+
+#     Disk.CallCmd('deleteDisk', 2, {})
+#     # Disk.CallCmd("readFromDisk", 1, {'blockNo':10})
+#     # Disk.CallCmd("", 2, {'size':200})
+#     Disk.CallCmd('deleteDisk', 1, {})
+#     printState(Disk)
