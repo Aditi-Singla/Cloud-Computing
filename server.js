a1c64a7427c787131ce631085bf74422729a31a2
Aditi <cs1140205@iitd.ac.in>
2017-11-12 19:14:02 +0530

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/code'));
+
+app.get('/',function(req,res){
+
+    console.log('hello from server');
+
+     res.render('./code/index.html');
+
+
+});
+
+app.listen(port);
+
+console.log('Server Listening at port '+port);
